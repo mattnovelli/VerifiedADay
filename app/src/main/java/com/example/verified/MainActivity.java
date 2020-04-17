806d67077a90073d0c2c92c6edaa59c07dac3639
aditya <aditya.fuldeore@gmail.com>
2020-04-17 17:16:32 -0500

    Button code?

--- app/src/main/java/com/example/verified/MainActivity.java
+++ app/src/main/java/com/example/verified/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.verified;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     }
     Button tweetButton = (Button) findViewById(R.id.tweetButton);
     Button newPersonButton = (Button) findViewById(R.id.newPersonButton);
+
+    //click new person button?
+    public String clicked() {
+        //clicks button
+        newPersonButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+        if (newPersonButton.isPressed()) {
+            return "verified";
+        }
+        return "yesterdays user??";
+    }
+    //send tweet?
+    public String tweet() {
+        if (clicked().equals("verified")) {
+            tweetButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                }
+            });
+            return "tweet";
+        }
+        return null;
+    }
     
 }
