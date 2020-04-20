ea5de50e63df7f66c2ca32dda99e8057516f5ee4
aditya <aditya.fuldeore@gmail.com>
2020-04-20 16:57:15 -0500

    volley library added

--- app/src/main/java/com/example/verified/MainActivity.java
+++ app/src/main/java/com/example/verified/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.verified;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +19,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button tweetButton = (Button) findViewById(R.id.tweetButton);
     }
+    RequestQueue queue = Volley.newRequestQueue(this);
+    StringRequest string =
+    /*Button newPersonButton = (Button) findViewById(R.id.newPersonButton);
+
+    //click new person button?
+    public String clicked() {
+        //clicks button
+        newPersonButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+        if (newPersonButton.isPressed()) {
+            return "verified";
+        }
+        return "yesterdays user??";
+    }
+    //send tweet?
+    public String tweet() {
+        if (clicked().equals("verified")) {
+            tweetButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                }
+            });
+            return "tweet";
+        }
+        return null;
+    }*/
 
 }
